e3fa8e6e5584791ba76b9e6645ef192a96b9ccec
Anele Twala <114112536+Anele0722@users.noreply.github.com>
2023-11-16 12:45:04 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
               An all-in-one digital identity solution for issuing and managing student ID cards. Perfect for time, attendance register, and identification verification.
             </p>
             <button className="bg-gradient-to-r from-orange-600 to-purple-600 py-3 px-6 text-lg rounded-md w-48">
-               <a href="index.html" style={{ color: 'white', textDecoration: 'none' }}>Learn More</a>   
+            Learn More
             </button>
           </div>
         </div>
@@ -51,6 +51,19 @@ function App() {
               blockchain to ensure that your identity is as secure as Fort Knox. Say goodbye to identity theft worries.
             </p>
           </div>
+               <div className="bg-gradient-to-r from-orange-600 to-purple-600 py-4 text-white text-center">
+      <h2 className="text-xl font-bold">Subscribe for Updates</h2>
+      <p className="text-sm">Stay up to date with our latest features and announcements.</p>
+      <input
+        type="email"
+        placeholder="Enter your email"
+        className="rounded-md px-4 py-2"/>
+      <button className="bg-white text-black rounded-md px-4 py-2 ml-2">
+        Subscribe
+      </button>
+    </div>
+
+  
         </div>
       </div>
     </div>
