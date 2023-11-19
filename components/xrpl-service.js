6de7d329e6fecada626089a5442b582f5c514362
Anele Twala <114112536+Anele0722@users.noreply.github.com>
2023-11-19 10:59:52 +0200

    Update xrpl-service.js

--- components/xrpl-service.js
+++ components/xrpl-service.js
@@ -1,35 +1,37 @@
-const express = require('express');
-const { RippleAPI } = require('ripple-lib');
+// XrpService.js
+import React, { useState } from 'react';
+import { RippleAPI } from 'ripple-lib';
 
-const app = express();
-const port = 3000;
+const XrpService = ({ account }) => {
+  const [accountInfo, setAccountInfo] = useState(null);
 
-app.get('/:account', async (req, res) => {
-  const account = req.params.account;
+  const connectToXRP = async () => {
+    const api = new RippleAPI({ server: 'wss://s.altnet.rippletest.net:51233' });
+    await api.connect();
 
-  // Connect to the XRP Ledger
-  const api = new RippleAPI({ server: 'wss://s.altnet.rippletest.net:51233' });
-  await api.connect();
+    try {
+      const accountInfo = await api.getAccountInfo(account);
+      setAccountInfo(accountInfo);
+    } catch (error) {
+      console.error('Error:', error.message);
+    } finally {
+      await api.disconnect();
+    }
+  };
 
-  try {
-    // Get account information
-    const accountInfo = await api.getAccountInfo(account);
+  return (
+    <div>
+      <h3 className="text-2xl font-bold mb-2">XRPL Service</h3>
+      <button onClick={connectToXRP}>Check Account Info</button>
+      {accountInfo && (
+        <div>
+          <p className="text-lg">Account: {accountInfo.account}</p>
+          <p className="text-lg">Balance: {accountInfo.xrpBalance} XRP</p>
+          <p className="text-lg">Sequence: {accountInfo.sequence}</p>
+        </div>
+      )}
+    </div>
+  );
+}
 
-    res.json({
-      account: accountInfo.account,
-      balance: accountInfo.xrpBalance,
-      sequence: accountInfo.sequence,
-      // Add more relevant information based on your use case
-    });
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-  } finally {
-    // Disconnect from the XRP Ledger
-    await api.disconnect();
-  }
-});
-
-app.listen(port, () => {
-  console.log(`XRPL Service is running at http://localhost:${port}`);
-});
+export default XrpService;
