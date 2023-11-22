c697e622c9400897c763222ddeff364bd6fca1ff
Anele Twala <114112536+Anele0722@users.noreply.github.com>
2023-11-22 08:36:43 +0200

    Update Verifiable-Credential.js

--- components/Verifiable-Credential.js
+++ components/Verifiable-Credential.js
@@ -6,10 +6,14 @@ import vc from 'vc-js';
 
 const VerifiableCredential = () => {
   useEffect(() => {
+    // Function to create a Verifiable Credential
     const createVerifiableCredential = async () => {
+      // Generate an Ed25519 key pair for the credential
       const keyPair = await Ed25519KeyPair.generate();
+      // Form the issuer DID using the generated key pair
       const issuerDID = `did:key:${keyPair.id}`;
 
+      // Defining the Verifiable Credential data
       const credential = {
         '@context': ['https://www.w3.org/2018/credentials/v1'],
         type: ['VerifiableCredential'],
@@ -20,22 +24,28 @@ const VerifiableCredential = () => {
         },
       };
 
+      // Issue the Verifiable Credential with a digital signature
       const signedVC = await vc.issue({
         credential,
         suite: new Ed25519Signature2018({ key: keyPair }),
         documentLoader: vc.documentLoader,
       });
 
+      // Convert the signed Verifiable Credential to JSON format
       const signedVCJson = JSON.stringify(signedVC, null, 2);
 
+      // Logging the generated Verifiable Credential to the console
       console.log('Verifiable Credential:');
       console.log(signedVCJson);
     };
 
+    // Call the function to create a Verifiable Credential
     createVerifiableCredential();
-  }, []);
+  }, []); // The empty dependency array ensures the effect runs only once on mount
 
-  return null; // or render component UI if needed
+  // Return null or render component UI if needed
+  return null;
 }
 
+// Export the VerifiableCredential component
 export default VerifiableCredential;
