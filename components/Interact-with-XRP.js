377d3d112cab4074d72c08b34060a54c9d4e23d4
Anele Twala <114112536+Anele0722@users.noreply.github.com>
2023-11-22 08:32:09 +0200

    Update Interact-with-XRP.js

--- components/Interact-with-XRP.js
+++ components/Interact-with-XRP.js
@@ -1,21 +1,24 @@
 // XrpBalance.js
 import React, { useEffect, useState } from 'react';
-import XrpJsInteraction from './xrp';
+import XrpJsInteraction from './xrp'; //importing the XrpJsInteraction
 
 const XrpBalance = () => {
-  const [xrpBalance, setXrpBalance] = useState(null);
+  const [xrpBalance, setXrpBalance] = useState(null); //state that holds the XRP balance
 
-  useEffect(() => {
-    XrpJsInteraction.checkXRPBalance('your-xrp-wallet-address').then((balance) => {
-      setXrpBalance(balance);
+  useEffect(() => { // useEffect hook to fetch the XRP balance when the component mounts
+    XrpJsInteraction.checkXRPBalance('your-xrp-wallet-address').then((balance) => { // Calling the checkXRPBalance function from XrpJsInteraction
+                                                                                    // Replace 'your-xrp-wallet-address' with the actual wallet address
+      setXrpBalance(balance);// Updating the state with the fetched balance
     });
-  }, []);
+  }, []); //The empty dependency array ensures the effect runs only once on mount
+
 
   return (
-    <div>
-      <h3 className="text-2xl font-bold mb-2">XRP Wallet Balance</h3>
-      {xrpBalance !== null ? (
-        <p className="text-lg">Balance: {xrpBalance} XRP</p>
+    <div> {/* Header displaying the component title */}
+      <h3 className="text-2xl font-bold mb-2">XRP Wallet Balance</h3>     
+      {xrpBalance !== null ? (       {/* Displaying XRP balance or loading message based on the state */}
+
+        <p className="text-lg">Balance: {xrpBalance} XRP</p>     
       ) : (
         <p className="text-lg">Loading XRP balance...</p>
       )}
